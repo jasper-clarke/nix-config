15fb3aae2dc40689f1df9f4886db84d3ce56fd3c
allusive-dev <jasper@allusive.dev>
2023-12-10 17:37:57 +1100

    cleanup

--- setup/scripts/SpotiStatus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pctl=$(playerctl --player=spotify status)
-if  [[ $pctl = *"Playing"* ]]; then
-    playerctl --player=spotify metadata title
-elif [[ $pctl = *"Paused"* ]]; then
-    echo "Paused"
-else
-    echo ""
-fi
--- setup/scripts/idea.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-_JAVA_AWT_WM_NONREPARENTING=1 idea
