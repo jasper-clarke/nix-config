23fd04dae5641284e4f76beedfb269c744c3283e
jasper-at-windswept <jasper@windswept.digital>
2024-02-11 18:30:14 +1100

    full system update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -51,8 +51,8 @@
 
   nix = {
     settings = {
-      substituters = [ "https://ezkea.cachix.org" ];
-      trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+      # substituters = [ "https://ezkea.cachix.org" ];
+      # trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
       experimental-features = [ "nix-command" "flakes" ];
     };
   };
